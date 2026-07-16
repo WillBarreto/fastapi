7850950a3065f3e99df19ab9e6d04698a9be82f7
WillBarreto <will.rescatador@gmail.com>
2026-07-16 12:50:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from zoneinfo import ZoneInfo
 from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, Boolean, ForeignKey, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session, relationship
-from sqlalchemy.sql import funcdef
+from sqlalchemy.sql import func
 from fastapi.responses import HTMLResponse
 import requests
 import json
